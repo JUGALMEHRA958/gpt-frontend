4cf018d881d61d5cda7e727bdb3a360ad11f7456
Jugal Mehra <jugal.mehra@indianic.com>
2023-07-24 17:13:50 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast";
+import { themeSettings } from "./theme";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Summary from "./pages/Summary";
+
+// import Summary from "./pages/Summary";
+// import Paragraph from "./pages/Paragraph";
+// import ChatBot from "./pages/ChatBot";
+// import JsConverter from "./pages/JsConverter";
+// import ScifiImage from "./pages/ScifiImage";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings()), []);
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/summary" element={<Summary />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import { Box, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const theme = useTheme("");
   return (
     <div>
-      <h1>Nav menu</h1>
+      <Box
+        width={"100%"}
+        p="1rem 6%"
+        textAlign={"center"}
+        backgroundColor={theme.palette.background.alt}
+        sx={{ boxShadow: 3, mb: 2 }}
+      >
+        <Typography variant="h1" color={"primary"} fontWeight={"bold"}>
+          GPT project
+        </Typography>
+        <Link to="/register" p={1}>
+          register
+        </Link>
+        <Link to="/login" p={1}>
+          login
+        </Link>
+      </Box>
     </div>
   );
 };
